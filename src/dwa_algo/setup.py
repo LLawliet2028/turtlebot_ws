044691e642d3f5c88a3cefb7146600856bd3f85d
Ranjan Mishra <109550148+LLawliet2028@users.noreply.github.com>
2025-11-02 18:39:37 +0530

    updated

--- src/dwa_algo/setup.py
+++ src/dwa_algo/setup.py
@@ -24,36 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'dwa_planner = dwa_algo.dwa_planner:main',
-        ],
-    },
-
-from setuptools import setup
-import os
-from glob import glob
-
-package_name = 'dwa_algo'
-
-setup(
-    name=package_name,
-    version='1.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Ranjan',
-    maintainer_email='ranjanmishra2028@gmail.com',
-    description='Custom DWA Local Planner for TurtleBot3 Navigation',
-    license='Apache-2.0',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'dwa_planner = dwa_algo.dwa_planner:main',
+            'goal_publisher = dwa_algo.goal_publisher:main',
         ],
     },
 )
-
